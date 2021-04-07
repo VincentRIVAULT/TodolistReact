59cbe38437465a058f90d86ce2057daa51829865
VincentRIVAULT <rivaultv@gmail.com>
2021-04-07 16:14:08 +0200

    Maj todolist from CRA

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Todolist from './Todolist';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
