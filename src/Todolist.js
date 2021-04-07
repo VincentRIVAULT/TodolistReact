b69eb96b3e586ed0c3895bf41242486f10531289
VincentRIVAULT <rivaultv@gmail.com>
2021-04-07 15:59:31 +0200

    ajout base todolist from Create React App

---
+++ src/Todolist.js
@@ -0,0 +1,16 @@
+import './Todolist.css';
+
+function Todolist() {
+    return (
+      <div className="Todolist">
+        <header className="Todolist-header">
+            <h1>Ma Todolist !</h1>
+        </header>
+        <section className="Todolist-section">
+            <h2>Ma liste de courses :</h2>
+        </section>
+      </div>
+    );
+  }
+  
+  export default Todolist;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Todolist from './Todolist';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Todolist />
   </React.StrictMode>,
   document.getElementById('root')
 );
