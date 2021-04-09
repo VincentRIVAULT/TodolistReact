323e43907313a3d68abb33e5ead27300f211daa5
VincentRIVAULT <rivaultv@gmail.com>
2021-04-09 15:14:03 +0200

    Maj todolist React

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 // eslint-disable-next-line no-unused-vars
-import App from './components/App.js';
-import Todolist from './components/Todolist.js';
+import App from './components/App';
+import Todolist from './components/Todolist';
 import 'semantic-ui-css/semantic.min.css'
 
 
